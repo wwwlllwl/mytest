6bda21c52fe419cde65f4c640d19cd8cdcf3cd4d
wl <2280555867@qq.com>
2018-11-12 18:48:50 +0800

    添加test工程到git

---
+++ idea_git_test/src/main/java/com/itheima/gitTest/GitTest.java
@@ -0,0 +1,10 @@
+package com.itheima.gitTest;
+
+public class GitTest {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 500;
+
+        System.out.println(a+b);
+    }
+}
