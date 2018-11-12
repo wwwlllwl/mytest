43b48193a6a00e3ddc0b450bbcbd04a34440c8ce
wl <2280555867@qq.com>
2018-11-12 20:48:12 +0800

    第一次修改test工程到git

--- idea_git_test/src/main/java/com/itheima/gitTest/GitTest.java
+++ idea_git_test/src/main/java/com/itheima/gitTest/GitTest.java
@@ -4,6 +4,9 @@ public class GitTest {
     public static void main(String[] args) {
         int a = 100;
         int b = 500;
+        for (int i = 0; i <5 ; i++) {
+            a+=100;
+        }
 
         System.out.println(a+b);
     }
